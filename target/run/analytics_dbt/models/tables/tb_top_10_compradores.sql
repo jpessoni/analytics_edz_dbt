f3b4b07f863ba0851293d332c0001a3a3e7ca9d8
jpessoni <jpessoni@gmail.com>
2023-12-03 21:58:36 -0300

    commit_tabela_top10
    Inclusão da tabela top_10_compradores

---
+++ target/run/analytics_dbt/models/tables/tb_top_10_compradores.sql
@@ -0,0 +1,36 @@
+
+  
+    
+
+  create  table
+    "dev"."public"."tb_top_10_compradores__dbt_tmp"
+    
+    
+    
+  as (
+    with sales as(
+	select * from vw_sales
+	--"dev"."public"."vw_sales"
+),
+
+compradores as (
+	select
+		(firstname||' '|| lastname) as Nome_Completo, userid
+	from users
+),
+
+top_10_compradores as(
+	select 
+	  compradores.Nome_completo as Nome_completo,
+	  sum(sales.quantidade_vendida) as Vendas_totais,
+	  sales.comissao as Comissao
+	from sales inner join compradores
+    on sales.id_comprador = compradores.userid
+    group by compradores.userid, compradores.Nome_completo, sales.comissao
+    order by Vendas_totais desc
+    limit 10
+)
+
+select * from top_10_compradores
+  );
+  
